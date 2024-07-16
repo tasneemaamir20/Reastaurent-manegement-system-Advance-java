d93543f9e3768e0c051f69923deb17e2fdb672e2
Tasneem Aamir <117052484+tasneemaamir20@users.noreply.github.com>
2024-07-16 12:41:57 +0530

    Add files via upload

---
+++ src/main/java/config/ControllerConfig.java
@@ -0,0 +1,8 @@
+package restaurent_management_system.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages= {"controller"})
+public class ControllerConfig {
+
+}
---
+++ src/main/java/config/PojoConfig.java
@@ -0,0 +1,8 @@
+package restaurent_management_system.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages= {"restaurent_management_system.model","service"})
+public class PojoConfig {
+
+}
